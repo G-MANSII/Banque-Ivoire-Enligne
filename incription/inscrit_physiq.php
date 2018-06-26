ad0e5d1d9f5570bd613d9e4946ef2e6af7551b9d
Alha13 <alhassaneo@gmail.com>
2018-06-26 19:33:31 +0000

    inscri fini

---
+++ incription/inscrit_physiq.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<title>Nouveau compte - Morale</title>
+<meta name="description" content="" Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" l'une="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" l'une="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "="">
+<meta name="keywords" content="online bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FRonline bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FR">
+<meta name="author" content="CCS - Computer Consulting Services">
+<meta name="generator" content="CCS - Computer Consulting Services">
+<link href="../logo.ico" rel="shortcut icon" type="image/x-icon">
+<link href="../logo.png" rel="apple-touch-icon" sizes="500x380">
+<link href="../css/font-awesome.min.css" rel="stylesheet">
+<link href="../css/BIO_V1.css" rel="stylesheet">
+<link href="../css/inscrit_physiq.css" rel="stylesheet">
+<script src="../java-script/jquery-2.2.4.min.js"></script>
+<script src="../java-script/wwb11.min.js"></script>
+<script>   
+   $(document).ready(function()
+   {
+      $("#Nav .navbar a").hover(function()
+      {
+         if ($(this).hasClass('active'))
+            return;
+         $(this).children("span").stop().fadeTo(500, 0);
+      }, function()
+      {
+         $(this).children("span").stop().fadeTo(500, 1);
+      })
+   });
+</script>
+</head>
+<body>
+   <div id="container">
+      <div id="foot">
+         <div id="foot_Container">
+         </div>
+      </div>
+      <div id="Layer2">
+         <hr id="Line5">
+         <input type="submit" id="btnvalide" name="valide" value="Validé">
+         <div id="Layer5">
+            <select name="titre" size="1" id="Combobox4" tabindex="0">
+            </select>
+            <input type="text" id="Editbox34" name="precision" value="" tabindex="1" placeholder="Pr&#233;cision*">
+            <div id="wb_Text11">
+               <div id="wb_uid0"><span id="wb_uid1">Autre</span></div>
+            </div>
+            <input type="radio" id="RadioButton4" onmousedown="ShowObject('Editbox34', 1);return false;" name="Name1" value="on">
+            <div id="wb_Text12">
+               <div id="wb_uid2"><span id="wb_uid3">Epargne</span></div>
+            </div>
+            <input type="radio" id="RadioButton5" name="Name1" value="on" required="">
+            <div id="wb_Text13">
+               <div id="wb_uid4"><span id="wb_uid5">Courant</span></div>
+            </div>
+            <input type="radio" id="RadioButton6" name="Name1" value="on">
+            <div id="wb_Text14">
+               <div id="wb_uid6"><span id="wb_uid7">Type de compte:</span></div>
+            </div>
+            <div id="wb_Text15">
+               <div id="wb_uid8"><span id="wb_uid9">Agence d'ouverture:</span></div>
+            </div>
+            <div id="wb_Text17">
+               <div id="wb_uid10"><span id="wb_uid11"><strong><u>Identification compte</u></strong></span></div>
+            </div>
+         </div>
+         <div id="wb_Text9">
+            <div id="wb_uid12"><span id="wb_uid13">Tous les champs doivent être correctement remplis sous penne d'être rejeté.</span></div>
+         </div>
+         <div id="Layer1">
+            <div id="wb_Text6">
+               <div id="wb_uid14"><span id="wb_uid15">Titre: </span></div>
+            </div>
+            <select name="titre" size="1" id="Combobox1" tabindex="0" autofocus="">
+               <option value="M.">M.*</option>
+               <option value="mme">Mme*</option>
+               <option value="mlle">Mlle*</option>
+               <option value="dr">Dr.*</option>
+               <option value="prof">Prof.*</option>
+            </select>
+            <input type="text" id="Editbox2" name="edtnam" value="" tabindex="1" placeholder="Nom*">
+            <input type="text" id="Editbox3" name="edtprenom" value="" tabindex="2" placeholder="Pr&#233;nom*">
+            <input type="radio" id="RadioButton2" name="Name" value="on" required="">
+            <div id="wb_Text3">
+               <div id="wb_uid16"><span id="wb_uid17">CNI</span></div>
+            </div>
+            <input type="radio" id="RadioButton1" name="Name" value="on">
+            <input type="radio" id="RadioButton3" onmousedown="ShowObject('pieceautre', 1);return false;" name="Name" value="on">
+            <div id="wb_Text4">
+               <div id="wb_uid18"><span id="wb_uid19">Passeport</span></div>
+            </div>
+            <div id="wb_Text5">
+               <div id="wb_uid20"><span id="wb_uid21">Autre</span></div>
+            </div>
+            <input type="text" id="pieceautre" name="edtnam" value="" tabindex="1" placeholder="Titre*">
+            <input type="text" id="Editbox4" name="edtnam" value="" tabindex="1" placeholder="N&#176; de pi&#232;ce d&#39;identit&#233;*">
+            <div id="wb_Text2">
+               <div id="wb_uid22"><span id="wb_uid23">Mode identification:</span></div>
+            </div>
+            <input type="date" id="Editbox1" name="naissance" value="" tabindex="5" placeholder="Date de naissance*">
+            <input type="text" id="Editbox5" name="lieu" value="" tabindex="7" placeholder="Lieu de naissance*">
+            <input type="text" id="Editbox6" name="adresspost" value="" tabindex="8" placeholder="Adresse postale">
+            <input type="text" id="Editbox7" name="profession" value="" tabindex="1" placeholder="Profession*">
+            <input type="tel" id="Editbox9" name="tel1" value="" tabindex="1" placeholder="T&#233;l&#233;phone 1*">
+            <input type="tel" id="Editbox10" name="tel2" value="" tabindex="1" placeholder="T&#233;l&#233;phone 2">
+            <input type="tel" id="Editbox11" name="fix" value="" tabindex="1" placeholder="Fix">
+            <div id="wb_Text1">
+               <div id="wb_uid24"><span id="wb_uid25">Sexe: </span></div>
+            </div>
+            <select name="titre" size="1" id="Combobox2" tabindex="0" autofocus="">
+               <option value="M">M</option>
+               <option value="F">F</option>
+            </select>
+            <input type="text" id="Editbox22" name="nationalite" value="" tabindex="1" placeholder="Nationalit&#233;*">
+            <input type="text" id="Editbox36" name="pays" value="" tabindex="1" placeholder="Pays de r&#233;sidence*">
+            <div id="wb_Text16">
+               <div id="wb_uid26"><span id="wb_uid27"><strong><u>Identification client - Informations personnelles </u></strong></span></div>
+            </div>
+            <div id="wb_Text18">
+               <div id="wb_uid28"><span id="wb_uid29">Situation matrimoniale: </span></div>
+            </div>
+            <select name="titre" size="1" id="Combobox5" tabindex="0" autofocus="">
+            </select>
+            <input type="file" id="FileUpload1" name="FileUpload1">
+            <input type="text" id="Editbox23" name="conjoint" value="" tabindex="2" placeholder="Conjoint">
+            <input type="text" id="Editbox35" name="ville" value="" tabindex="1" placeholder="Ville de r&#233;sidence*">
+         </div>
+      </div>
+      <div id="Layer4">
+         <div id="wb_Shape2">
+            <img src="../images/img0108.png" id="Shape2" alt=""></div>
+         <label for="" id="Label3">Ouverture de compte</label>
+      </div>
+      <div id="wb_Breadcrumb2">
+         <ul id="Breadcrumb2">
+            <li><a href="./../administrator/conf/Categorie_de_poste.php" title="xx"><i class="fa fa-home">&nbsp;</i>Acceuil</a></li>
+            <li><a href="./inscription.html" title="&lt;x"><i class="fa fa-inbox">&nbsp;</i>Inscription</a></li>
+            <li class="active"><i class="fa fa-address-card-o">&nbsp;</i> Client physique</li>
+         </ul>
+      </div>
+      <div id="Div">
+         <div id="head">
+            <div id="wb_band">
+               <img src="../images/img0001.png" id="band" alt="Banque Ivoire Online" title="Banque Ivoire Online"></div>
+            <input type="text" id="edit_reche" name="search" value="" placeholder="Recherche...">
+            <div id="wb_icosear">
+               <a href="./../administrator/conf/Categorie_de_poste.php"><div id="icosear"><i class="fa fa-search">&nbsp;</i></div></a></div>
+            <div id="wb_local">
+               <div id="wb_uid30"><span id="wb_uid31"><a href="./../administrator/conf/Categorie_de_poste.php">Banque prêt de chez vous&nbsp;!</a></span></div>
+            </div>
+            <div id="wb_icolocal">
+               <div id="icolocal"><i class="fa fa-map-marker">&nbsp;</i></div></div>
+            <div id="wb_txtacc">
+               <div id="wb_uid32"><span id="wb_uid33"><em>La banque en ligne faite pour vous&nbsp;!</em></span></div>
+            </div>
+            <div id="wb_logo">
+               <a href="./../index.php"><img src="../images/logo.png" id="logo" alt=""></a></div>
+         </div>
+         <div id="Nav">
+            <ul class="navbar">
+               <li><a href="./../index.php"><img alt="Acceuil" title="Acceuil" src="../images/img0003_over.png"><span><img alt="Acceuil" title="Acceuil" src="../images/img0003.png"></span></a></li>
+               <li><a href="./../comptes/"><img alt="Comptes" title="Comptes" src="../images/img0027_over.png"><span><img alt="Comptes" title="Comptes" src="../images/img0027.png"></span></a></li>
+               <li><a href="./../prets/"><img alt="Pr&#234;ts" title="Pr&#234;ts" src="../images/img0044_over.png"><span><img alt="Pr&#234;ts" title="Pr&#234;ts" src="../images/img0044.png"></span></a></li>
+               <li><a href="./../agences/"><img alt="Agences" title="Agences" src="../images/img0045_over.png"><span><img alt="Agences" title="Agences" src="../images/img0045.png"></span></a></li>
+               <li><a href="./../contact/contact.php"><img alt="Contacts" title="Contacts" src="../images/img0046_over.png"><span><img alt="Contacts" title="Contacts" src="../images/img0046.png"></span></a></li>
+               <li><a href="./../a_propos/"><img alt="A propos" title="A propos" src="../images/img0047_over.png"><span><img alt="A propos" title="A propos" src="../images/img0047.png"></span></a></li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</body>
+</html>
