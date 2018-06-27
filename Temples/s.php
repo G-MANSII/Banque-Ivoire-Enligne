72586572323b95a080230e54f9e1b2efc690aa83
Alha13 <alhassaneo@gmail.com>
2018-06-27 13:15:03 +0000

    aj

--- Temples/s.php
+++ Temples/s.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Untitled Page</title>
-<meta name="description" content="" Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" lune="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "="">
+<meta name="description" content="" Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" l'une="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "="">
 <meta name="keywords" content="online bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FR">
 <meta name="author" content="CCS - Computer Consulting Services">
 <meta name="generator" content="CCS - Computer Consulting Services">
@@ -14,45 +14,6 @@
 </head>
 <body>
    <div id="container">
-      <div id="Html1">
-         <select id="colors" onchange="changeColor()">
-            <option value="1">Red</option>
-            <option value="2">Blue</option>
-            <option value="3">Green</option>
-            <option value="4">Yellow</option>
-            <option value="5">Orange</option>
-            <option value="6" selected="selected">White</option>
-         </select>
-         <br><br>
-
-         <script>         
-         function changeColor() {
-         var eID = document.getElementById("colors");
-         var colorVal = eID.options[eID.selectedIndex].value;
-         var colortxt = eID.options[eID.selectedIndex].text;
-         
-         
-         if (colorVal==2) window.location.href="http://google.ci" ;
-         else window.location.href="http://sama.ci"
-         
-         }
-         </script></div>
-      <script>      
-      function changeColor() {
-      var eID = document.getElementById("colors");
-      var colorVal = eID.options[eID.selectedIndex].value;
-      var colortxt = eID.options[eID.selectedIndex].text;
-      
-      
-      if (colorVal==ville) window.location.href="http://google.ci" ;
-      else window.location.href="http://sama.ci"
-      
-      }
-      </script><select name="Combobox1" size="1" id="Combobox1" onchange="changeColor()">
-         <option value="ville">ville</option>
-         <option value="zd">zar</option>
-         <option value="dQS">dd</option>
-      </select>
    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Banque Ivoire Online</title>
-<meta name="description" content="" Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" lune="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" lune="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "="">
+<meta name="description" content="" Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" l'une="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" l'une="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "="">
 <meta name="keywords" content="online bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FRonline bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FR">
 <meta name="author" content="CCS - Computer Consulting Services">
 <meta name="generator" content="CCS - Computer Consulting Services">
@@ -31,7 +31,7 @@
          pagination: false,
          effectlength: 2000
       });
-      $("#NavigationBar1 .navbar a").hover(function()
+      $("#Nav .navbar a").hover(function()
       {
          if ($(this).hasClass('active'))
             return;
@@ -49,73 +49,78 @@
          <div id="foot_Container">
          </div>
       </div>
-      <div id="zone">
-         <div id="Slider">
-            <img class="image" src="images/logo.png" alt="" title="">
-            <img class="image" src="images/im-B000672_hp-billboard_965x325.FINAL.ReachforGreat_new.jpg" id="wb_uid0" alt="" title="">
-            <img class="image" src="images/OFFR.png" id="wb_uid1" alt="" title="">
-            <img class="image" src="images/Prize.jpg" id="wb_uid2" alt="" title="">
+      <div id="Slider">
+         <img class="image" src="images/logo.png" alt="" title="">
+         <img class="image" src="images/im-B000672_hp-billboard_965x325.FINAL.ReachforGreat_new.jpg" id="wb_uid0" alt="" title="">
+         <img class="image" src="images/OFFR.png" id="wb_uid1" alt="" title="">
+         <img class="image" src="images/Prize.jpg" id="wb_uid2" alt="" title="">
+      </div>
+      <div id="divhg">
+         <div id="wb_btn_connexacei">
+            <a href="./connexion/connexion.html"><div id="btn_connexacei"><div id="btn_connexacei_text"><span id="wb_uid3"><strong>Connexion</strong></span></div></div></a></div>
+         <div id="wb_btn_inscriptiacei">
+            <a href="./incription/inscription.html"><div id="btn_inscriptiacei"><div id="btn_inscriptiacei_text"><span id="wb_uid4"><strong>Inscription</strong></span></div></div></a></div>
+         <div id="wb_txt_AC">
+            <div id="wb_uid5"><span id="wb_uid6">Connectez vous et gérez </span></div>
+            <div id="wb_uid7"><span id="wb_uid8">votre compte&nbsp;!</span></div>
          </div>
-         <div id="divhg">
-            <div id="wb_btn_connexacei">
-               <a href="./connexion/connexion.php"><div id="btn_connexacei"><div id="btn_connexacei_text"><span id="wb_uid3"><strong>Connexion</strong></span></div></div></a></div>
-            <div id="wb_btn_inscriptiacei">
-               <a href="./incription/inscription.html"><div id="btn_inscriptiacei"><div id="btn_inscriptiacei_text"><span id="wb_uid4"><strong>Inscription</strong></span></div></div></a></div>
-            <div id="wb_txt_AC">
-               <div id="wb_uid5"><span id="wb_uid6">Connectez vous et gérez </span></div>
-               <div id="wb_uid7"><span id="wb_uid8">votre compte&nbsp;!</span></div>
-            </div>
-            <div id="wb_txtpass">
-               <div id="wb_uid9"><span id="wb_uid10">Mot de passe oublier&nbsp;?</span></div>
-            </div>
-            <div id="wb_txtins">
-               <div id="wb_uid11"><span id="wb_uid12">Vous avez pas de compte&nbsp;? </span></div>
-               <div id="wb_uid13"><span id="wb_uid14">Creer en 1 min&nbsp;!</span></div>
-            </div>
+         <div id="wb_txtpass">
+            <div id="wb_uid9"><span id="wb_uid10">Mot de passe oublier&nbsp;?</span></div>
          </div>
-         <div id="wb_txt_pkoi">
-            <div id="wb_uid15"><span id="wb_uid16">Banks are supposed to be respected institutions. But what really matters to us, is being respectful. At </span></div>
-            <div id="wb_uid17"><span id="wb_uid18">Santander Bank, we want you to prosper. That’s why we treat you and your money with respect. From </span></div>
-            <div id="wb_uid19"><span id="wb_uid20">how we talk and listen, to how we act and how we care for the person behind the account number. </span></div>
-            <div id="wb_uid21"><span id="wb_uid22">And it’s not just about the big stuff. Every little interaction and connection matters. Because it all adds </span></div>
-            <div id="wb_uid23"><span id="wb_uid24">up to something great.</span></div>
-            <div id="wb_uid25"><span id="wb_uid26">Banks are supposed to be respected institutions. But what really matters to us, is being respectful. At </span></div>
-            <div id="wb_uid27"><span id="wb_uid28">Santander Bank, we want you to prosper. That’s why we treat you and your money with respect. From </span></div>
-            <div id="wb_uid29"><span id="wb_uid30">how we talk and listen, to how we act and how we care for the person behind the account number. </span></div>
-            <div id="wb_uid31"><span id="wb_uid32">And it’s not just about the big stuff. Every little interaction and connection matters. Because it all adds </span></div>
-            <div id="wb_uid33"><span id="wb_uid34">up to something great.</span></div>
+         <div id="wb_txtins">
+            <div id="wb_uid11"><span id="wb_uid12">Vous avez pas de compte&nbsp;? </span></div>
+            <div id="wb_uid13"><span id="wb_uid14">Creer en 1 min&nbsp;!</span></div>
          </div>
-         <div id="divcenter">
-            <div id="wb_Muha3">
-               <img src="images/M ali 2.jpg" id="Muha3" alt=""></div>
-            <label for="" id="lba">Le compte Epargne</label>
-            <div id="wb_Text1">
-               <div id="wb_uid35"><span id="wb_uid36">En 1967, trois ans après avoir remporté </span></div>
-               <div id="wb_uid37"><span id="wb_uid38">le championnat des poids lourds, il est </span></div>
-               <div id="wb_uid39"><span id="wb_uid40">critiqué pour son refus d'être enrôlé dans </span></div>
-               <div id="wb_uid41"><span id="wb_uid42">l'armée américaine p mondial et de sa </span></div>
-               <div id="wb_uid43"><span id="wb_uid44">licence de boxe. Il ne combat plus </span></div>
-               <div id="wb_uid45"><span id="wb_uid46">pendant près de quatre ans, jusqu'à ce </span></div>
-               <div id="wb_uid47"><span id="wb_uid48">que son appel soit finalement reçu par la </span></div>
-               <div id="wb_uid49"><span id="wb_uid50">cour suprême américaine.</span></div>
-            </div>
+      </div>
+      <div id="divcenter">
+         <div id="wb_Muha3">
+            <img src="images/M ali 2.jpg" id="Muha3" alt=""></div>
+         <label for="" id="lba">Le compte Epargne</label>
+         <div id="wb_Text1">
+            <div id="wb_uid15"><span id="wb_uid16">En 1967, trois ans après avoir remporté </span></div>
+            <div id="wb_uid17"><span id="wb_uid18">le championnat des poids lourds, il est </span></div>
+            <div id="wb_uid19"><span id="wb_uid20">critiqué pour son refus d'être enrôlé dans </span></div>
+            <div id="wb_uid21"><span id="wb_uid22">l'armée américaine p mondial et de sa </span></div>
+            <div id="wb_uid23"><span id="wb_uid24">licence de boxe. Il ne combat plus </span></div>
+            <div id="wb_uid25"><span id="wb_uid26">pendant près de quatre ans, jusqu'à ce </span></div>
+            <div id="wb_uid27"><span id="wb_uid28">que son appel soit finalement reçu par la </span></div>
+            <div id="wb_uid29"><span id="wb_uid30">cour suprême américaine.</span></div>
          </div>
+      </div>
+      <div id="wb_Breadcrumb2">
+         <ul id="Breadcrumb2">
+            <li class="active"><i class="fa fa-home">&nbsp;</i>Acceuil</li>
+         </ul>
+      </div>
+      <div id="Layer1">
          <div id="txt_pk">
             <hr id="Line2e">
             <div id="wb_Headi">
                <h1 id="Headi">POURQUOI NOUS COISIR ?</h1></div>
             <hr id="Line1a">
          </div>
+         <div id="wb_txt_pkoi">
+            <div id="wb_uid31"><span id="wb_uid32">Banks are supposed to be respected institutions. But what really matters to us, is being respectful. At </span></div>
+            <div id="wb_uid33"><span id="wb_uid34">Santander Bank, we want you to prosper. That’s why we treat you and your money with respect. From </span></div>
+            <div id="wb_uid35"><span id="wb_uid36">how we talk and listen, to how we act and how we care for the person behind the account number. </span></div>
+            <div id="wb_uid37"><span id="wb_uid38">And it’s not just about the big stuff. Every little interaction and connection matters. Because it all adds </span></div>
+            <div id="wb_uid39"><span id="wb_uid40">up to something great.</span></div>
+            <div id="wb_uid41"><span id="wb_uid42">Banks are supposed to be respected institutions. But what really matters to us, is being respectful. At </span></div>
+            <div id="wb_uid43"><span id="wb_uid44">Santander Bank, we want you to prosper. That’s why we treat you and your money with respect. From </span></div>
+            <div id="wb_uid45"><span id="wb_uid46">how we talk and listen, to how we act and how we care for the person behind the account number. </span></div>
+            <div id="wb_uid47"><span id="wb_uid48">And it’s not just about the big stuff. Every little interaction and connection matters. Because it all adds </span></div>
+            <div id="wb_uid49"><span id="wb_uid50">up to something great.</span></div>
+         </div>
       </div>
-      <div id="divhead">
+      <div id="Div">
          <div id="head">
             <div id="wb_band">
                <img src="images/img0001.png" id="band" alt="Banque Ivoire Online" title="Banque Ivoire Online"></div>
             <input type="text" id="edit_reche" name="search" value="" placeholder="Recherche...">
             <div id="wb_icosear">
-               <a href="./index1.php"><div id="icosear"><i class="fa fa-search">&nbsp;</i></div></a></div>
+               <a href="./administrator/conf/Categorie_de_poste.php"><div id="icosear"><i class="fa fa-search">&nbsp;</i></div></a></div>
             <div id="wb_local">
-               <div id="wb_uid51"><span id="wb_uid52"><a href="./index1.php">Banque prêt de chez vous&nbsp;!</a></span></div>
+               <div id="wb_uid51"><span id="wb_uid52"><a href="./administrator/conf/Categorie_de_poste.php">Banque prêt de chez vous&nbsp;!</a></span></div>
             </div>
             <div id="wb_icolocal">
                <div id="icolocal"><i class="fa fa-map-marker">&nbsp;</i></div></div>
@@ -125,7 +130,7 @@
             <div id="wb_logo">
                <a href="./index.php"><img src="images/logo.png" id="logo" alt=""></a></div>
          </div>
-         <div id="NavigationBar1">
+         <div id="Nav">
             <ul class="navbar">
                <li><a href="./index.php"><img alt="Acceuil" title="Acceuil" src="images/img0003_over.png"><span><img alt="Acceuil" title="Acceuil" src="images/img0003.png"></span></a></li>
                <li><a href="./comptes/"><img alt="Comptes" title="Comptes" src="images/img0027_over.png"><span><img alt="Comptes" title="Comptes" src="images/img0027.png"></span></a></li>
