4950512017a95df6352fe3e58b03a2ee47a364b9
Alha13 <alhassaneo@gmail.com>
2018-06-29 23:51:47 +0000

    new

---
+++ grh/conf/Gestionnaires.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<title>Absences</title>
+<meta name="description" content="" Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" lune="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "Demandez="" un="" compte="" bancaire="" en="" ligne="" auprès="" de="" la="" banque="" Santander="" et="" profitez="" des="" options="" de="" compte="" en="" ligne="" pratiques="" de="" lune="" des="" meilleures="" banques="" personnelles."lang=" fr-FR" "="">
+<meta name="keywords" content="online bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FRonline bank(s), personal banking, bank account(s), best personal banks, apply for bank account online,banque (s) en ligne, banque personnelle, compte (s) bancaire (s), meilleures banques personnelles, demande de compte bancaire en ligne " lang=" fr-FR">
+<meta name="author" content="CCS - Computer Consulting Services">
+<meta name="generator" content="CCS - Computer Consulting Services">
+<link href="../../logo.ico" rel="shortcut icon" type="image/x-icon">
+<link href="../../logo.png" rel="apple-touch-icon" sizes="500x380">
+<link href="../../css/font-awesome.min.css" rel="stylesheet">
+<link href="../../css/BIO_V1.css" rel="stylesheet">
+<link href="../../css/Gestionnaires.css" rel="stylesheet">
+<script src="../../java-script/jquery-1.12.4.min.js"></script>
+<script src="../../java-script/wwb11.min.js"></script>
+<script>   
+   $(document).ready(function()
+   {
+      $("#Nav .navbar a").hover(function()
+      {
+         if ($(this).hasClass('active'))
+            return;
+         $(this).children("span").stop().fadeTo(500, 0);
+      }, function()
+      {
+         $(this).children("span").stop().fadeTo(500, 1);
+      })
+   });
+</script>
+<!-- Start of biohelp Zendesk Widget script -->
+<script>   /*<![CDATA[*/window.zE||(function(e,t,s){var n=window.zE=window.zEmbed=function(){n._.push(arguments)}, a=n.s=e.createElement(t),r=e.getElementsByTagName(t)[0];n.set=function(e){ n.set._.push(e)},n._=[],n.set._=[],a.async=true,a.setAttribute("charset","utf-8"), a.src="https://static.zdassets.com/ekr/asset_composer.js?key="+s, n.t=+new Date,a.type="text/javascript",r.parentNode.insertBefore(a,r)})(document,"script","920064d9-b4f5-46ec-b48c-bef256a7e182");/*]]>*/
+</script>
+<!-- End of biohelp Zendesk Widget script -->
+</head>
+<body>
+   <div id="container">
+      <div id="foot">
+         <div id="foot_Container">
+            <div id="wb_JavaScript1">
+               <div id="copyrightnotice"></div>
+               <script>               
+                  var now = new Date();
+                  var startYear = "2017";
+                  var text =  "Copyright &copy; ";
+                  if (startYear != '')
+                  {
+                     text = text + startYear + "-";
+                  }
+                  text = text + now.getFullYear() + ", Miage 2018. Tous droits réservés.";
+                  var copyrightnotice = document.getElementById('copyrightnotice');
+                  copyrightnotice.innerHTML = text;
+               </script>
+
+            </div>
+         </div>
+      </div>
+      <div id="wb_Breadcrumb2">
+         <ul id="Breadcrumb2">
+            <li><a href="./../../index.php" title="xx"><i class="fa fa-home">&nbsp;</i>Acceuil</a></li>
+            <li><a href="./../grh.php" title="Gestions des resources humaines"><i class="fa fa-database">&nbsp;</i>Gestions des resources humaines</a></li>
+            <li><a href="./../grh.php" title="Config"><i class="fa fa-window-restore">&nbsp;</i>Config</a></li>
+            <li class="active"><i class="fa fa-ship">&nbsp;</i>Gestionnaires</li>
+         </ul>
+      </div>
+      <div id="Layer2">
+         <div id="wb_Shape2">
+            <img src="../../images/img0099.png" id="Shape2" alt=""></div>
+         <label for="" id="Label3">Gestionnaires</label>
+      </div>
+      <form name="Layer1" method="post" action="" enctype="text/plain" id="Layer1">
+         <label for="" id="Label2">GRH - Gestionnaires</label>
+         <div id="Layer4">
+            <div id="wb_Text7">
+               <div id="wb_uid0"><span id="wb_uid1">Titre: </span></div>
+            </div>
+            <select name="titre" size="1" id="Combobox3" tabindex="0" autofocus="">
+               <option value="M.">M.*</option>
+               <option value="mme">Mme*</option>
+               <option value="mlle">Mlle*</option>
+               <option value="dr">Dr.*</option>
+               <option value="prof">Prof.*</option>
+            </select>
+            <input type="text" id="Editbox15" name="edtnam" value="" tabindex="1" placeholder="Nom*">
+            <input type="text" id="Editbox16" name="edtprenom" value="" tabindex="2" placeholder="Pr&#233;nom*">
+            <input type="radio" id="RadioButton4" name="Name" value="on" required="">
+            <div id="wb_Text8">
+               <div id="wb_uid2"><span id="wb_uid3">CNI</span></div>
+            </div>
+            <input type="radio" id="RadioButton5" name="Name" value="on">
+            <input type="radio" id="RadioButton6" onmousedown="ShowObject('pieceautre', 1);return false;" name="Name" value="on">
+            <div id="wb_Text9">
+               <div id="wb_uid4"><span id="wb_uid5">Passeport</span></div>
+            </div>
+            <div id="wb_Text10">
+               <div id="wb_uid6"><span id="wb_uid7">Autre</span></div>
+            </div>
+            <input type="text" id="Editbox17" name="titrpiece" value="" tabindex="1" placeholder="Titre*">
+            <input type="text" id="Editbox18" name="numeropiece" value="" tabindex="1" placeholder="N&#176; de pi&#232;ce d&#39;identit&#233;*">
+            <div id="wb_Text11">
+               <div id="wb_uid8"><span id="wb_uid9">Mode identification:</span></div>
+            </div>
+            <input type="date" id="Editbox19" name="naissance" value="" tabindex="5" placeholder="Date de naissance*">
+            <input type="text" id="Editbox20" name="lieu" value="" tabindex="7" placeholder="Lieu de naissance*">
+            <input type="text" id="Editbox21" name="adresspost" value="" tabindex="8" placeholder="Adresse postale">
+            <input type="tel" id="Editbox25" name="tel1" value="" tabindex="1" placeholder="T&#233;l&#233;phone 1*">
+            <input type="tel" id="Editbox26" name="tel2" value="" tabindex="1" placeholder="T&#233;l&#233;phone 2">
+            <input type="tel" id="Editbox27" name="fix" value="" tabindex="1" placeholder="Fix">
+            <div id="wb_Text12">
+               <div id="wb_uid10"><span id="wb_uid11">Sexe: </span></div>
+            </div>
+            <select name="sexe" size="1" id="Combobox4" tabindex="0" autofocus="">
+               <option value="M">M</option>
+               <option value="F">F</option>
+            </select>
+            <input type="text" id="Editbox28" name="nationalite" value="" tabindex="1" placeholder="Nationalit&#233;*">
+            <input type="text" id="Editbox29" name="pays" value="" tabindex="1" placeholder="Pays de r&#233;sidence*">
+            <div id="wb_Text13">
+               <div id="wb_uid12"><span id="wb_uid13"><strong><u>Identification client - Informations personnelles </u></strong></span></div>
+            </div>
+            <div id="wb_Text14">
+               <div id="wb_uid14"><span id="wb_uid15">Situation matrimoniale: </span></div>
+            </div>
+            <select name="matrimoniale" size="1" id="Combobox6" tabindex="0" autofocus="">
+               <option value="Célibataire">Célibataire</option>
+               <option value="Marié(e)">Marié(e)</option>
+               <option value="Divorcé(e)">Divorcé(e)</option>
+               <option value="Veuf(ve)">Veuf(ve)</option>
+            </select>
+            <input type="file" id="FileUpload2" name="FileUpload1">
+            <input type="text" id="Editbox30" name="conjoint" value="" tabindex="2" placeholder="Conjoint(e)">
+            <input type="text" id="Editbox31" name="ville" value="" tabindex="1" placeholder="Ville de r&#233;sidence*">
+            <div id="wb_Text1">
+               <div id="wb_uid16"><span id="wb_uid17">Photo:</span></div>
+            </div>
+            <input type="text" id="Editbox4" name="nommere" value="" tabindex="1" autocomplete="off" placeholder="Nom complet de la m&#232;re*">
+            <input type="text" id="Editbox3" name="nompere" value="" tabindex="1" autocomplete="off" placeholder="Nom complet du p&#232;re*">
+         </div>
+         <div id="Layer6">
+            <div id="wb_Text21">
+               <div id="wb_uid18"><span id="wb_uid19">Pièces d'identification:</span></div>
+            </div>
+            <div id="wb_Text23">
+               <div id="wb_uid20"><span id="wb_uid21"><strong><u>Documents à fournir</u></strong></span></div>
+            </div>
+            <input type="file" id="FileUpload1" name="piece">
+            <input type="file" id="FileUpload3" name="cv">
+            <div id="wb_Text2">
+               <div id="wb_uid22"><span id="wb_uid23">Curriculum Vitae</span></div>
+            </div>
+            <div id="wb_Text19">
+               <div id="wb_uid24"><span id="wb_uid25">Lettre de motivation:</span></div>
+            </div>
+            <input type="file" id="FileUpload4" name="lettre">
+            <input type="text" id="Editbox5" name="gestion" value="Gestion clients" tabindex="1" readonly="" autocomplete="off" placeholder="Gestion clients">
+            <input type="text" id="Editbox6" name="fonction" value="Gestionnaire" tabindex="1" readonly="" autocomplete="off" placeholder="Fonction*">
+            <input type="text" id="Editbox7" name="poste" value="Gestions" tabindex="1" readonly="" autocomplete="off" placeholder="Poste*">
+         </div>
+         <input type="submit" id="btnconnexion" name="Ajouter" value="Ajouter">
+      </form>
+      <div id="Divi">
+         <div id="head">
+            <div id="wb_band">
+               <img src="../../images/img0001.png" id="band" alt="Banque Ivoire Online" title="Banque Ivoire Online"></div>
+            <input type="text" id="edit_reche" name="search" value="" placeholder="Recherche...">
+            <div id="wb_icosear">
+               <a href="./../../administrator/conf/Categorie_de_poste.php"><div id="icosear"><i class="fa fa-search">&nbsp;</i></div></a></div>
+            <div id="wb_local">
+               <div id="wb_uid26"><span id="wb_uid27"><a href="./../../agences/">Banque prêt de chez vous&nbsp;!</a></span></div>
+            </div>
+            <div id="wb_icolocal">
+               <div id="icolocal"><i class="fa fa-map-marker">&nbsp;</i></div></div>
+            <div id="wb_txtacc">
+               <div id="wb_uid28"><span id="wb_uid29"><em>La banque en ligne faite pour vous&nbsp;!</em></span></div>
+            </div>
+            <div id="wb_logo">
+               <a href="./../../index.php"><img src="../../images/logo.png" id="logo" alt=""></a></div>
+         </div>
+         <div id="Nav">
+            <ul class="navbar">
+               <li><a href="./../../index.php"><img alt="Acceuil" title="Acceuil" src="../../images/img0003_over.png"><span><img alt="Acceuil" title="Acceuil" src="../../images/img0003.png"></span></a></li>
+               <li><a href="./../../comptes/"><img alt="Comptes" title="Comptes" src="../../images/img0027_over.png"><span><img alt="Comptes" title="Comptes" src="../../images/img0027.png"></span></a></li>
+               <li><a href="./../../prets/"><img alt="Pr&#234;ts" title="Pr&#234;ts" src="../../images/img0044_over.png"><span><img alt="Pr&#234;ts" title="Pr&#234;ts" src="../../images/img0044.png"></span></a></li>
+               <li><a href="./../../agences/"><img alt="Agences" title="Agences" src="../../images/img0045_over.png"><span><img alt="Agences" title="Agences" src="../../images/img0045.png"></span></a></li>
+               <li><a href="./../../contact/contact.php"><img alt="Contacts" title="Contacts" src="../../images/img0046_over.png"><span><img alt="Contacts" title="Contacts" src="../../images/img0046.png"></span></a></li>
+               <li><a href="./../../a_propos/"><img alt="A propos" title="A propos" src="../../images/img0047_over.png"><span><img alt="A propos" title="A propos" src="../../images/img0047.png"></span></a></li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</body>
+</html>
