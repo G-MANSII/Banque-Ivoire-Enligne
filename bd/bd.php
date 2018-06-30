e1d7057359f4d3aa1e0e4793a6936432a858fe0d
Linda Yesso <35770033+LinYesso@users.noreply.github.com>
2018-06-30 13:14:53 +0000

    bd

--- bd/bd.php
+++ bd/bd.php
@@ -1,4 +1,14 @@
 <?php 
+	//constantes
+	define("HOST","localhost");
+	define("DB","banque");
+	define("USER","admin");
+	define("PASS","admin");
 
+	try{
+		$bd = new PDO("mysql:host=".HOST.";dbname=".DB,USER,PASS);
+	} catch (PDOException $e) {
+		die($e->getMessage());
+	}
 
  ?>
